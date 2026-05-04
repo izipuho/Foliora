e6ee1eb1a7fdeada81ebbde2573dcfcbb0c77c64
izipuho <58221294+izipuho@users.noreply.github.com>
2026-05-04 16:07:24 +0300

    new target, split photo analysis responsibility

---
+++ BellRecognition/BellRecognitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  BellRecognitionApp.swift
+//  BellRecognition
+//
+//  Created by Ivan Zipuho on 04.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct BellRecognitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BellRecognition/DomainEnums.swift
@@ -0,0 +1,8 @@
+//
+//  DomainEnums.swift
+//  BellRecognition
+//
+//  Created by Ivan Zipuho on 04.05.2026.
+//
+
+import Foundation
