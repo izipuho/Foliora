ecb7182c55cce78c6cc0fd1e0ae9a668e8f8ca6c
izipuho <58221294+izipuho@users.noreply.github.com>
2026-06-10 14:47:32 +0300

    remove statuses from sharing model

--- Catalog/Domain/Sharing/SharingModels.swift
+++ Catalog/Domain/Sharing/SharingModels.swift
@@ -8,19 +8,10 @@ enum CollectionAccessRole: String, Identifiable, Hashable, Codable {
     var id: String { rawValue }
 }
 
-enum CollectionParticipantStatus: String, Identifiable, Hashable, Codable {
-    case current
-    case removed
-    case unknown
-
-    var id: String { rawValue }
-}
-
 struct CollectionParticipant: Identifiable, Hashable, Codable {
     let id: UUID
     let collectionID: UUID
     let cloudKitParticipantID: String?
     let displayName: String?
     var role: CollectionAccessRole
-    var status: CollectionParticipantStatus
 }
