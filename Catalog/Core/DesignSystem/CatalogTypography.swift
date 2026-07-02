9357c2039f2e8694310fd4971b593601cf1a3bdd
izipuho <58221294+izipuho@users.noreply.github.com>
2026-07-02 13:41:32 +0300

    add catalog typography to design code

---
+++ Catalog/Core/DesignSystem/CatalogTypography.swift
@@ -0,0 +1,9 @@
+import SwiftUI
+
+enum CatalogTypography {
+    static let screenTitle = Font.largeTitle.weight(.bold)
+    static let sectionTitle = Font.headline
+    static let cardTitle = Font.title3.weight(.semibold)
+    static let cardSubtitle = Font.subheadline
+    static let chipLabel = Font.caption.weight(.semibold)
+}
