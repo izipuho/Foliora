01132ed887deb7fb7b50c39838ef2dd4b0f6fb9b
izipuho <58221294+izipuho@users.noreply.github.com>
2026-07-02 15:51:00 +0300

    remove legacy comments

--- Catalog/Core/DesignSystem/CatalogColors.swift
+++ Catalog/Core/DesignSystem/CatalogColors.swift
@@ -28,31 +28,3 @@ enum CatalogMediaContrast {
 
     static let onMediaPrimary = Color.white
 }
-
-//enum CatalogMediaContrast {
-//    static let coverScrimBottom = Color.black.opacity(0.22)
-//    static let coverScrimTop = Color.black.opacity(0.02)
-//    static let overlayChip = Color.white.opacity(0.16)
-//    static let overlayChipMuted = Color.white.opacity(0.72)
-//    static let glassStroke = Color.white.opacity(0.32)
-//    static let mediaSelectionStroke = Color.white.opacity(0.9)
-//    static let previewGradientStart = Color.white.opacity(0.88)
-//    static let previewGradientEnd = Color.white.opacity(0.72)
-//    static let mapScrimTop = Color.black.opacity(0)
-//    static let mapScrimMiddle = Color.black.opacity(0.10)
-//    static let mapScrimBottom = Color.black.opacity(0.30)
-//    static let iconPaletteShadowSoft = Color.black.opacity(0.25)
-//    static let iconPaletteShadowStrong = Color.black.opacity(0.35)
-//    static let onMediaPrimary = Color.white
-//}
-
-//enum CatalogSemanticColors {
-//    static let separator = Color(uiColor: .separator)
-//    static let groupedSurface = Color(uiColor: .secondarySystemGroupedBackground)
-//    static let groupedSurfaceElevated = Color(uiColor: .tertiarySystemGroupedBackground)
-//    static let fill = Color(uiColor: .systemFill)
-//    static let secondaryFill = Color(uiColor: .secondarySystemFill)
-//    static let tertiaryFill = Color(uiColor: .tertiarySystemFill)
-//    static let quaternaryFill = Color(uiColor: .quaternarySystemFill)
-//    static let tertiaryLabel = Color(uiColor: .tertiaryLabel)
-//}
