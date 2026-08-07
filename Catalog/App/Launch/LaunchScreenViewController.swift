6f1ff7dec11e56837d641cbc37d4d90244552273
izipuho <58221294+izipuho@users.noreply.github.com>
2026-08-07 13:57:36 +0300

    Resolve launch screen views by tag

--- Catalog/App/Launch/LaunchScreenViewController.swift
+++ Catalog/App/Launch/LaunchScreenViewController.swift
@@ -1,6 +1,24 @@
 import UIKit
 
 final class LaunchScreenViewController: UIViewController {
+    private enum Tag {
+        static let medallionBell = 100
+        static let medallion = 101
+        static let symbol = 102
+    }
+
+    private var MedallionBell: UIView {
+        view.viewWithTag(Tag.medallionBell)!
+    }
+
+    private var Medallion: UIImageView {
+        view.viewWithTag(Tag.medallion) as! UIImageView
+    }
+
+    private var Symbol: UIImageView {
+        view.viewWithTag(Tag.symbol) as! UIImageView
+    }
+
     static func instantiate( storyboardName: String, bundle: Bundle = .main ) -> LaunchScreenViewController? {
         let storyboard = UIStoryboard(name: storyboardName, bundle: bundle);
         return storyboard.instantiateInitialViewController { coder in LaunchScreenViewController(coder: coder) }
