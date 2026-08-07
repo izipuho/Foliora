4503fb872fc93d61a5da503c31c5286a8928e0de
izipuho <58221294+izipuho@users.noreply.github.com>
2026-08-07 13:51:45 +0300

    Add launch screen storyboard instantiation

--- Catalog/App/Launch/LaunchScreenViewController.swift
+++ Catalog/App/Launch/LaunchScreenViewController.swift
@@ -1,4 +1,8 @@
 import UIKit
 
 final class LaunchScreenViewController: UIViewController {
+    static func instantiate( storyboardName: String, bundle: Bundle = .main ) -> LaunchScreenViewController? {
+        let storyboard = UIStoryboard(name: storyboardName, bundle: bundle);
+        return storyboard.instantiateInitialViewController { coder in LaunchScreenViewController(coder: coder) }
+    }
 }
