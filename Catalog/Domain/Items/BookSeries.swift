9b6f7c92710903bf39fd56a06926204db63d2e20
izipuho <58221294+izipuho@users.noreply.github.com>
2026-08-26 23:15:30 +0300

    Add book series domain model

---
+++ Catalog/Domain/Items/BookSeries.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+/// Represents a book series scoped to a single collection.
+struct BookSeries: Identifiable, Hashable, Codable {
+    let id: UUID
+    let collectionID: UUID
+    var name: String
+    var totalBookCount: Int?
+}
