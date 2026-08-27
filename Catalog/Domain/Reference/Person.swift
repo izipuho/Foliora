9587497b65dd2a8fea52615a86ecf92d42e2b584
izipuho <58221294+izipuho@users.noreply.github.com>
2026-08-27 13:43:45 +0300

    Add person domain identity

--- Catalog/Domain/Reference/Person.swift
+++ Catalog/Domain/Reference/Person.swift
@@ -1,7 +1,8 @@
 import Foundation
 
 /// Represents person data and behavior.
-struct Person: Hashable, Codable {
+struct Person: Identifiable, Hashable, Codable {
+    let id: UUID
     var name: String
     var birthYear: Int?
     var deathYear: Int?
