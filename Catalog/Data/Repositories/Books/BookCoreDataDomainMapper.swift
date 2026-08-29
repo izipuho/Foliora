4a9a161d7f64aed3c05edc91bd27479884918a87
izipuho <58221294+izipuho@users.noreply.github.com>
2026-08-29 23:09:00 +0300

    Map singular publisher logo

--- Catalog/Data/Repositories/Books/BookCoreDataDomainMapper.swift
+++ Catalog/Data/Repositories/Books/BookCoreDataDomainMapper.swift
@@ -69,15 +69,16 @@ extension CoreDataDomainMapper {
         )
 
         let locationEntity = entity.value(forKey: "location") as? NSManagedObject
-        let logos = relatedObjects(entity, "logos")
+        let logo = relatedObjects(entity, "logos")
             .sorted { intValue($0, "sortOrder") < intValue($1, "sortOrder") }
+            .first
             .map { mediaAsset(from: $0) }
 
         return Publisher(
             id: uuidValue(entity, "id"),
             name: stringValue(entity, "name"),
             location: locationEntity.map { place(from: $0) },
-            logos: logos
+            logo: logo
         )
     }
 
