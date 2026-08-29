27a2cf1739c845e3e48085b3decc450340c487c2
izipuho <58221294+izipuho@users.noreply.github.com>
2026-08-29 23:22:57 +0300

    Remove publisher logo compatibility

--- Catalog/Domain/Reference/Publisher.swift
+++ Catalog/Domain/Reference/Publisher.swift
@@ -18,46 +18,4 @@ struct Publisher: Identifiable, Hashable, Codable {
         self.location = location
         self.logo = logo
     }
-
-    /// Keeps existing preview fixtures source-compatible while the domain uses a singular logo.
-    init(
-        id: UUID,
-        name: String,
-        location: Place?,
-        logos: [MediaAsset]
-    ) {
-        self.init(
-            id: id,
-            name: name,
-            location: location,
-            logo: logos.sorted { $0.sortOrder < $1.sortOrder }.first
-        )
-    }
-
-    private enum CodingKeys: String, CodingKey {
-        case id
-        case name
-        case location
-        case logo
-        case logos
-    }
-
-    init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        id = try container.decode(UUID.self, forKey: .id)
-        name = try container.decode(String.self, forKey: .name)
-        location = try container.decodeIfPresent(Place.self, forKey: .location)
-        logo = try container.decodeIfPresent(MediaAsset.self, forKey: .logo)
-            ?? container.decodeIfPresent([MediaAsset].self, forKey: .logos)?
-                .sorted { $0.sortOrder < $1.sortOrder }
-                .first
-    }
-
-    func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(id, forKey: .id)
-        try container.encode(name, forKey: .name)
-        try container.encodeIfPresent(location, forKey: .location)
-        try container.encodeIfPresent(logo, forKey: .logo)
-    }
 }
