fd8c806bc118d1579ddf15672cc3a1eae079d233
izipuho <58221294+izipuho@users.noreply.github.com>
2026-09-16 14:57:58 +0300

    Serialize default multi-photo Vision analysis

---
+++ Catalog/Core/PhotoAnalysis/DefaultPhotoAnalysisService+MultiPhoto.swift
@@ -0,0 +1,19 @@
+import CoreGraphics
+
+extension DefaultPhotoAnalysisService {
+    /// Avoids running several already-parallel Vision pipelines at the same time.
+    func analyze(images: [CGImage]) async -> MultiPhotoAnalysisResult {
+        guard !images.isEmpty else {
+            return .empty
+        }
+
+        var results: [PhotoAnalysisResult] = []
+        results.reserveCapacity(images.count)
+
+        for image in images {
+            results.append(await analyze(image: image))
+        }
+
+        return MultiPhotoAnalysisResult(photos: results)
+    }
+}
