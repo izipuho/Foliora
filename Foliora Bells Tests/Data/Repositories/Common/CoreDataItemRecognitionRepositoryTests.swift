c23d18e8ba13b2da1021a8b7d75524943527abde
izipuho <58221294+izipuho@users.noreply.github.com>
2026-09-21 09:57:24 +0300

    Restore recognition evidence repository tests

--- Foliora Bells Tests/Data/Repositories/Common/CoreDataItemRecognitionRepositoryTests.swift
+++ Foliora Bells Tests/Data/Repositories/Common/CoreDataItemRecognitionRepositoryTests.swift
@@ -17,6 +17,7 @@ struct CoreDataItemRecognitionRepositoryTests {
         let record = ItemRecognitionRecord(
             itemID: itemID,
             photoAssetIDs: [firstPhotoID, secondPhotoID],
+            evidenceData: Data([0x01, 0x02]),
             resultData: Data([0x03, 0x04]),
             updatedAt: updatedAt
         )
@@ -35,6 +36,7 @@ struct CoreDataItemRecognitionRepositoryTests {
         let updatedRecord = ItemRecognitionRecord(
             itemID: itemID,
             photoAssetIDs: [secondPhotoID],
+            evidenceData: Data([0x05]),
             resultData: Data([0x06]),
             schemaVersion: 2,
             updatedAt: updatedAt.addingTimeInterval(60)
@@ -60,6 +62,7 @@ struct CoreDataItemRecognitionRepositoryTests {
         let record = ItemRecognitionRecord(
             itemID: UUID(),
             photoAssetIDs: [UUID()],
+            evidenceData: Data([0x01]),
             resultData: Data([0x02])
         )
 
